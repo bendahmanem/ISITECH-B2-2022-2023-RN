e7269f72c9bb7fa84eedc5e0902a5552eeaf7b7d
Mounir BENDAHMANE <mounir.bendahmane@ecole-isitech.fr>
2023-04-17 15:59:43 +0200

    lib: react-navigation/native installation

---
+++ b2-rn/views/Second.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+function Second() {
+  return (
+    <View>
+        <Text>Welcome to the second component</Text>
+    </View>
+  )
+}
+
+export default Second
