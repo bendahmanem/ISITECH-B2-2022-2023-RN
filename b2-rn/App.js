95f20f16e4875895a297878859001b904b03f7a0
Mounir BENDAHMANE <mounir.bendahmane@ecole-isitech.fr>
2023-04-17 17:05:45 +0200

    feat: navigation

--- b2-rn/App.js
+++ b2-rn/App.js
@@ -1,63 +1,20 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeView from './views/HomeView'
+import SecondView from './views/SecondView';
+// Navigation stack setup
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
-  const [enteredTodo, setEnteredTodo] = useState('test')
-
-  const changeTextHandler = (pEnteredTodo) => {
-    setEnteredTodo(pEnteredTodo)
-  }
-
-  const changeTodoHandler = () => {
-    console.log(enteredTodo)
-  }
 
   return (
-    <View style={styles.containerFlex}>
-      <View style={styles.blueFlexItem}>
-      <Text>Blue</Text>
-      </View>
-      <View style={styles.greenFlexItem}>
-      <Text>{enteredTodo}</Text>
-      <TextInput 
-        placeholder='Entrer nouvelle valeur de state'
-        onChangeText={changeTextHandler}
-       />
-       <Button
-        onPress={changeTodoHandler}
-        title='Create todo' /> 
-      </View>
-      <View style={styles.yellowFlexItem} >
-      <Text>Yellow</Text>
-      </View>
-      
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Accueil' component={HomeView} options={{title: 'Accueil'}} />
+          <Stack.Screen name='Second' component={SecondView} options={{title: 'Second'}}/>
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  containerFlex: {
-    flex: 1,
-  },
-  blueFlexItem : {
-    flex: 2,
-    backgroundColor: 'blue'
-  },
-  greenFlexItem : {
-    flex: 5,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  yellowFlexItem : {
-    flex: 2,
-    backgroundColor: 'yellow'
-  },
-});
---
+++ b2-rn/views/HomeView.js
@@ -0,0 +1,67 @@
+import { StatusBar } from "expo-status-bar"
+import { useState } from "react"
+import { Button, StyleSheet, Text, TextInput, View } from "react-native"
+
+const HomeView = () => {
+    const [enteredTodo, setEnteredTodo] = useState('test')
+
+    const changeTextHandler = (pEnteredTodo) => {
+        setEnteredTodo(pEnteredTodo)
+    }
+
+    const changeTodoHandler = () => {
+        console.log(enteredTodo)
+    }
+
+    return (
+        <View style={styles.containerFlex}>
+            <View style={styles.blueFlexItem}>
+                <Text>Blue</Text>
+            </View>
+            <View style={styles.greenFlexItem}>
+                <Text>{enteredTodo}</Text>
+                <TextInput
+                    placeholder='Entrer nouvelle valeur de state'
+                    onChangeText={changeTextHandler}
+                />
+                <Button
+                    onPress={changeTodoHandler}
+                    title='Create todo' />
+            </View>
+            <View style={styles.yellowFlexItem} >
+                <Text>Yellow</Text>
+            </View>
+
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+
+export default HomeView
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'red',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    containerFlex: {
+      flex: 1,
+    },
+    blueFlexItem: {
+      flex: 2,
+      backgroundColor: 'blue'
+    },
+    greenFlexItem: {
+      flex: 5,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    yellowFlexItem: {
+      flex: 2,
+      backgroundColor: 'yellow'
+    },
+  });
+  
