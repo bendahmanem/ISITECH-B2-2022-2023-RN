59990b0d1647923229ad99cc636e6b227ad3b251
Mounir BENDAHMANE <mounir.bendahmane@ecole-isitech.fr>
2023-04-17 12:26:34 +0200

    feat: flex-example

--- b2-rn/App.js
+++ b2-rn/App.js
@@ -3,8 +3,17 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.containerFlex}>
+      <View style={styles.blueFlexItem}>
+      <Text>Blue</Text>
+      </View>
+      <View style={styles.greenFlexItem}>
+      <Text>Green</Text>
+      </View>
+      <View style={styles.yellowFlexItem} >
+      <Text>Yellow</Text>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  containerFlex: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'orange'
+  },
+  blueFlexItem : {
+    flex: 1,
+    backgroundColor: 'blue'
+  },
+  greenFlexItem : {
+    flex: 1,
+    backgroundColor: 'green'
+  },
+  yellowFlexItem : {
+    flex: 1,
+    backgroundColor: 'yellow'
   },
 });
