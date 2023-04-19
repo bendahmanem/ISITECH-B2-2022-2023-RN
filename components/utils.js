2fe2166bd4a2f8000babf94bb4c15c0be04a2495
Mounir BENDAHMANE <mounir.bendahmane@ecole-isitech.fr>
2023-04-19 09:39:36 +0200

    feat: sendmail utils, file utils

--- components/utils.js
+++ components/utils.js
@@ -3,8 +3,6 @@ import { readAsStringAsync} from 'expo-file-system'
 
 export const writeFile = async (data) => {
     // verifier le contenu de data 
-
-
     try {
         // verifier si le fichier est vide 
         const fileInfo = await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'file.txt')
@@ -18,4 +16,54 @@ export const writeFile = async (data) => {
     } catch (error) {
         console.log('erreur non catche writefile util.js: ', error)
     }
+}
+
+export const isFileEmpty = async (fileUri) => {
+    try {
+            const fileInfo = await FileSystem.getInfoAsync(fileUri);
+            console.log('isFileEmpty: ', fileInfo)
+            if (fileInfo.exists === true && fileInfo.size > 0) return false
+            return true
+    } catch (error) {
+        return true
+    }
+    return true
+}
+
+export const emptyFile = async (fileUri) => {
+    try {
+        await FileSystem.writeFileAsStringAsync(fileUri);
+        const fileContent = await FileSystem.readAsStringAsync(fileUri);
+
+    } catch (err) {
+        console.log('error in empty file function:', err)
+    }
+}
+
+const getFile = async (fileUri) => {
+    return await FileSystem.getInfoAsync(fileUri);
+}
+
+const getFileContent = async (fileUri) => {
+    return await FileSystem.readAsStringAsync(fileUri);
+}
+
+export const sendFileByEmail = async (recipient, fileUri) => {
+    let _recipient = recipient ? recipient : 'default@mail.com';
+    try {
+        const fileInfo = await getFile(fileUri)
+        console.log('file info before mail is sent: ', fileInfo);
+        const fileContent = await getFileContent(fileUri);
+        console.log('file content about to be sent by email: ', fileContent)
+    } catch(err) {
+        console.log('file does not exist: ', fileInfo)
+    }
+
+    const options = {
+        attachments: [fileUri],
+        recipients: [_recipient],
+        subject: 'Objet du mail'
+    }
+
+    await MailComposer.composeAsync(options)
 }
